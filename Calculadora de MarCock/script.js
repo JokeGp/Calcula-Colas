f06f415267e88d50ccea7275d09291754e1be98a
JokeGp <73568212+KikeX3@users.noreply.github.com>
2022-11-25 23:25:57 -0600

    Add files via upload

---
+++ Calculadora de MarCock/script.js
@@ -0,0 +1,13 @@
+let calcBtn = document.getElementById('calc');
+let resetBtn = document.getElementById('reset');
+let valueP = document.getElementById('value(p)');
+let valuePO = document.getElementById('value(PO)');
+let valueLq = document.getElementById('value(Lq)');
+let valueLs = document.getElementById('value(Ls)');
+let valueWq = document.getElementById('value(Wq)');
+let valueWs = document.getElementById('value(Ws)');
+let valuePn = document.getElementById('value(value((P(n >= s))))');
+
+let inputLa = document.getElementById('inputλ');
+let inputU = document.getElementById('inputμ');
+let inputN = document.getElementById('inputN');
